06fc40963065a8e40aac2a4ff70ad977f467ae5f
alaakorda <alaazakaria436@gmail.com>
2023-03-24 01:16:58 -0700

    isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -13,13 +13,6 @@
  {
          if(c >= 'A' && c <= 'Z')
              return (1);
-<<<<<<< HEAD
-        else if(c < 65 || c > 90)
-           return (0);
-        _putchar('\n');
-    }
-=======
         else
              return (0);     
-  }
->>>>>>> 31ad5d26d8b0f1495b43f81d4cbd9c23fb9c6593
+ }
